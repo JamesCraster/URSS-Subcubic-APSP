0ce38407d52f30fa1095adba5617015a04cfb5a2
JamesCraster <jamesvaughancraster@gmail.com>
2019-08-12 17:03:30 +0100

    finished code to split matrices

--- TChanMinPlus.py
+++ TChanMinPlus.py
@@ -1,2 +1,24 @@
+import math
+
+
+def splitMatrixHorizontal(A, d):
+    out = []
+    next = []
+    for x in range(0, math.ceil(len(A)/d)):
+        next = []
+        for y in range(0, len(A)):
+            next.append(A[y][x*d:min(math.ceil(x*d+d), len(A))])
+
+        out.append(next)
+    return out
+
+
+def splitMatrixVertical(A, d):
+    out = []
+    for x in range(0, math.ceil(len(A)/d)):
+        out.append(A[x*d:min(math.ceil(x*d+d), len(A))])
+    return out
+
+
 def TChanMinPlus(A, B):
     pass
