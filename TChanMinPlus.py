d3cd27fd4d9fa0375947933a519abecc845ced5b
JamesCraster <jamesvaughancraster@gmail.com>
2019-08-15 13:33:06 +0100

    distance product now produces an answer

--- TChanMinPlus.py
+++ TChanMinPlus.py
@@ -45,15 +45,6 @@ def dominates(A, B):
     return True
 
 
-def removeDuplicates(B):
-    A = copy.deepcopy(B)
-    for i in range(0, len(A)):
-        for j in range(i+1, len(A)):
-            if [A[i][y]['value'] for y in range(0, len(A[i]))] == [A[j][y]['value'] for y in range(0, len(A[i]))]:
-                del A[j]
-    return A
-
-
 def distanceProduct(A, B, n, d):
     C = [[10000]*n]*n
     for k in range(0, d):
@@ -66,8 +57,13 @@ def distanceProduct(A, B, n, d):
         Xk = dominatingPairs(first, second, d)
 
         print(Xk)
-        # for a in Xk:
-        #   C[a][b] = A[a.i][k] + B[k][b.i]
+        # extract i and j from Xk
+        for a in Xk:
+            i = a[0][0]['i']
+            j = a[1][0]['i']
+            print(i)
+            print(j)
+            C[i][j] = A[i][k] + B[k][j]
     return C
 
 
@@ -80,47 +76,16 @@ def dominatingPairs(A, B, d):
         return [(x, y) for x in A for y in B if dominates(y, x)]
     # sort both
     s = sorted(A+B, key=lambda x: x[d-1]['value'])
-    leftA = [x for x in A if x[d-1]['value'] < s[len(s)//2][d-1]['value']]
-    rightA = [x for x in A if x[d-1]['value'] >= s[len(s)//2][d-1]['value']]
-    leftB = [x for x in B if x[d-1]['value'] < s[len(s)//2][d-1]['value']]
-    rightB = [x for x in B if x[d-1]['value'] >= s[len(s)//2][d-1]['value']]
-    print(leftA)
-    print(leftB)
+    # how splitting works: elements equal to the median before the median are put into left
+    # elements equal to the median after the median, or at the median itself, are put into right
+    leftA = [A[x] for x in range(0, len(A)) if A[x][d-1]['value'] < s[len(s)//2]
+             [d-1]['value'] or (A[x][d-1] == s[len(s)//2][d-1]['value'] and x < len(s)//2)]
+    rightA = [A[x] for x in range(0, len(A)) if A[x][d-1]['value'] > s[len(s)//2]
+              [d-1]['value'] or (A[x][d-1]['value'] == s[len(s)//2][d-1]['value'] and x >= len(s)//2)]
+    leftB = [B[x] for x in range(0, len(B)) if B[x][d-1]['value'] < s[len(s)//2]
+             [d-1]['value'] or (B[x][d-1]['value'] == s[len(s)//2][d-1]['value'] and len(A) + x < len(s)//2)]
+    rightB = [B[x] for x in range(0, len(B)) if B[x][d-1]['value'] > s[len(s)//2]
+              [d-1]['value'] or (B[x][d-1]['value'] == s[len(s)//2][d-1]['value'] and len(A) + x >= len(s)//2)]
 
     return dominatingPairs(leftA, leftB, d) + dominatingPairs(rightA, rightB, d) \
         + dominatingPairs(leftA, rightB, d-1)
-
-
-def dominatingPairsInt(A, B, d):
-    if len(A) == 1 and len(B) == 1:
-        return [(x, y) for x in A for y in B if dominatesInt(y, x)]
-    if len(A) == 0 or len(B) == 0:
-        return []
-    if d == 0:
-        return [(x, y) for x in A for y in B if dominatesInt(y, x)]
-    # sort both
-    s = sorted(A+B, key=lambda x: x[d-1])
-    # concept: elements equal to the median before the median are put into left
-    # elements equal to the median after the median, or at the median itself, are put into right
-    leftA = [A[x] for x in range(0, len(A)) if A[x][d-1] < s[len(s)//2]
-             [d-1] or (A[x][d-1] == s[len(s)//2][d-1] and x < len(s)//2)]
-    rightA = [A[x] for x in range(0, len(A)) if A[x][d-1] > s[len(s)//2]
-              [d-1] or (A[x][d-1] == s[len(s)//2][d-1] and x >= len(s)//2)]
-    leftB = [B[x] for x in range(0, len(B)) if B[x][d-1] < s[len(s)//2]
-             [d-1] or (B[x][d-1] == s[len(s)//2][d-1] and len(A) + x < len(s)//2)]
-    rightB = [B[x] for x in range(0, len(B)) if B[x][d-1] > s[len(s)//2]
-              [d-1] or (B[x][d-1] == s[len(s)//2][d-1] and len(A) + x >= len(s)//2)]
-    print(leftA)
-    print(leftB)
-    print(rightA)
-    print(rightB)
-
-    return dominatingPairsInt(leftA, leftB, d) + dominatingPairsInt(rightA, rightB, d) \
-        + dominatingPairsInt(leftA, rightB, d-1)
-
-
-def dominatesInt(A, B):
-    for i in range(0, len(A)):
-        if(A[i] < B[i]):
-            return False
-    return True
