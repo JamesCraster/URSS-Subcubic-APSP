8bf0d4d29771bd955862b47c3804ff3fb1f1a7c2
JamesCraster <jamesvaughancraster@gmail.com>
2019-08-22 17:54:05 +0100

    calculated S

--- RWilliamsMinPlus.py
+++ RWilliamsMinPlus.py
@@ -7,23 +7,34 @@ def enforceUniqueness(A, B):
         for j in range(0, len(B[i])):
             B[i][j] = addByDoubling(B[i][j], len(A)+1)
     # Fredman's trick:
+    n = len(A)
+    d = len(A[0])
     Af = []
-    for i in range(0, len(A)):
+    for i in range(0, n):
         Af.append([])
-        for k in range(0, len(A[i])):
+        for k in range(0, d):
             Af[i].append([])
-            for otherK in range(0, len(A[i])):
+            for otherK in range(0, d):
                 Af[i][k].append(A[i][k]-A[i][otherK])
 
     Bf = []
-    for k in range(0, len(B)):
+    for k in range(0, d):
         Bf.append([])
-        for otherK in range(0, len(B)):
+        for otherK in range(0, d):
             Bf[k].append([])
-            for j in range(0, len(B[0])):
+            for j in range(0, n):
                 Bf[k][otherK].append(B[otherK][j]-B[k][j])
-
+    print(Af)
+    print(Bf)
     # Finish replacement:
+    S = []
+    for k in range(0, len(A[0])):
+        S.append([])
+        for otherK in range(0, len(A[0])):
+            new = [(Af[i][k][otherK], i, 'A')
+                   for i in range(0, n)] + [(Bf[k][otherK][i], i, 'B') for i in range(0, n)]
+            S[k].append(sorted(new))
+
     return (A, B)
 
 
